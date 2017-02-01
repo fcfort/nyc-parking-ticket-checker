152cc59845dda5c08e7fafb8193f046a9fec2781
Frank Fort <phranck+github@gmail.com>
2017-02-01 10:08:14 -0500

    accidentally removed html var

--- ticket_checker.py
+++ ticket_checker.py
@@ -19,8 +19,8 @@ soup = BeautifulSoup(br.response().read(), BEAUTIFUL_SOUP_PARSER)
 br.open(soup.body.iframe['src'])
 
 # Set violation #
-br.select_form(nr=0) # Form has no `name`
 
+br.select_form(nr=0) # Form has no `name`
 # Because there is both a non-mobile and mobile version on the page, we need
 # to find the first one and set it.
 br.find_control(name='args.VIOLATION_NUMBER_NOL', nr=0).value = args.violation
@@ -38,6 +38,7 @@ br.submit()
 
 # Look for violation response text
 
+html = br.response().read()
 soup = BeautifulSoup(br.response().read(), BEAUTIFUL_SOUP_PARSER)
 
 # Errors are put into a class `global-violation-prompt` div tag.
